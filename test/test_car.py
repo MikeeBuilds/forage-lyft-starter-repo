875d63233cf04729af46a63fbf19710eadbee2a6
MikeeBuilds <citizensofandrometta@gmail.com>
2023-12-30 16:35:32 -0600

    Fix spindler initialization in car factory, update tests

--- test/test_car.py
+++ test/test_car.py
@@ -1,14 +1,15 @@
 import sys
 print(sys.path)
 
-
 import unittest
-from datetime import datetime
+from datetime import datetime, date
 from engine.model.calliope import Calliope
 from engine.model.glissade import Glissade
 from engine.model.palindrome import Palindrome
 from engine.model.rorschach import Rorschach
 from engine.model.thovex import Thovex
+from engine.model.spindler import Spindler
+from engine.car_factory import CarFactory
 
 class TestCar(unittest.TestCase):
     def test_calliope_needs_service(self):
@@ -53,6 +54,26 @@ class TestCar(unittest.TestCase):
         last_service_mileage = 0
 
         car = Thovex(last_service_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
+        
+    def test_spindler_battery_needs_service(self):  
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+        current_mileage = 40000
+        last_service_mileage = 0
+
+        car = Spindler(last_service_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
+
+
+    def test_carrigan_tire_needs_service(self):  
+        tire_wear_array = [0.1, 0.3, 0.5, 0.95]
+        car = CarFactory.create_car("Spindler", "2023-01-01", 50000, 45000)
+        self.assertTrue(car.needs_service())
+
+    def test_octoprime_tire_needs_service(self):  
+        tire_wear_array = [0.8, 0.9, 0.5, 0.7]
+        car = CarFactory.create_car("Spindler", "2023-01-01", 50000, 45000)
         self.assertTrue(car.needs_service())
 
 if __name__ == '__main__':
